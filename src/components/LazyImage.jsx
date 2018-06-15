83ef1e409229d7f8a8ab62a858f4702d530fe552
Vo Thanh Dat <vothanhdat@outlook.com>
2018-06-16 01:19:35 +0700

    update

--- src/components/LazyImage.jsx
+++ src/components/LazyImage.jsx
@@ -1,10 +1,49 @@
 import React from 'react'
 
 
+export default class ImageLazyLoad extends React.Component {
+
+  _timeout = 0
+
+  state = {
+    islazy: true,
+    small: this.props.small,
+    large: this.props.large
+  }
 
 
+  static getDerivedStateFromProps(props, state) {
+    return {
+      islazy: props.small != state.small || props.large != state.large,
+      small: props.small,
+      large: props.large,
+    }
+  }
 
+  componentDidMount() {
+    this.lazyUpdate();
+  }
 
-export default class extends React.Component {
-    
+  componentDidUpdate(newProps, newState) {
+    newState.islazy && this.lazyUpdate();
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this._timeout);
+  }
+
+  lazyUpdate() {
+    clearTimeout(this._timeout);
+    this._timeout = setTimeout(() => this.setState({ islazy: false }), 50)
+  }
+
+  render() {
+    const { small, large, style = {}, ...rest } = this.props
+    const backgroundImage = this.state.islazy
+      ? `url(${small})`
+      : `url(${large}),url(${small})`
+
+    return <div style={{ ...style, backgroundImage }} {...rest}></div>
+  }
 }
+
--- src/pages/feed/StreamItem.jsx
+++ src/pages/feed/StreamItem.jsx
@@ -2,50 +2,9 @@
 import * as React from 'react'
 import withSCSS from "withsass.macro";
 import utils from '@/utils/utils'
+import LazyImage from '@/components/LazyImage'
 import { Link } from 'react-router-dom'
-
-class ImageLazyLoad extends React.Component {
-
-    state = { 
-        islazy: true, 
-        small: this.props.small, 
-        large: this.props.large 
-    }
-
-    _timeout = 0
-
-    static getDerivedStateFromProps(props, state) {
-        return {
-            islazy: props.small != state.small || props.large != state.large,
-            small: props.small,
-            large: props.large,
-        }
-    }
-
-    componentDidMount(){
-        this.lazyUpdate();
-    }
-
-    componentDidUpdate(newProps,newState) {
-        newState.islazy && this.lazyUpdate();
-    }
-
-    componentWillUnmount() {
-        clearTimeout(this._timeout);
-    }
-
-    lazyUpdate() {
-        clearTimeout(this._timeout);
-        this._timeout = setTimeout(() => this.setState({ islazy: false }), 50)
-    }
-
-    render() {
-        const { small, large, style = {}, ...rest } = this.props
-        return this.state.islazy
-            ? <div style={{ backgroundImage: `url(${small})`, ...style }} {...rest}></div>
-            : <div style={{ backgroundImage: `url(${large}),url(${small})`, ...style }} {...rest}></div>
-    }
-}
+import ButtonBase from '@material-ui/core/ButtonBase'
 
 @withSCSS('./StreamItem.scss')
 class StreamItem extends React.Component {
@@ -53,9 +12,9 @@ class StreamItem extends React.Component {
         const { _, data, classes, className } = this.props
         const imgLinkSmall = utils.getImageURL(data.avatar, '50')
         const imgLink = utils.getImageURL(data.avatar, '300')
-        return <Link to={`/live/${data.uid}`} className={`${classes.item} ${className}`}>
-            <ImageLazyLoad className={classes.img} small={imgLinkSmall} large={imgLink}/>
-        </Link>
+        return <ButtonBase component={Link} to={`/live/${data.uid}`} className={`${classes.item} ${className}`}>
+            <LazyImage className={classes.img} small={imgLinkSmall} large={imgLink}/>
+        </ButtonBase>
     }
 
 }
