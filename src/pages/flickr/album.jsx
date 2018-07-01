694dfceea1010e2dd6821dc2c25360bf3f006ff7
Vo Thanh Dat <vothanhdat@outlook.com>
2018-07-01 18:13:04 +0700

    mp

--- src/pages/flickr/album.jsx
+++ src/pages/flickr/album.jsx
@@ -1,17 +1,13 @@
 //@ts-check
 import React from 'react'
 import withSCSS from 'withsass.macro'
-import { FlickUser } from '@/store/connects/flickr'
+import { FlickAlbum } from '@/store/connects/flickr'
 import PhotoListView from './PhotoListView';
 import { get as getpath } from 'lodash'
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import LangLink from '@/components/Link';
-import SwipeableViews from 'react-swipeable-views';
-import AlbumListView from './AlbumListView';
 
-@FlickUser()
-class UserStream extends React.Component {
+
+@FlickAlbum()
+class Album extends React.Component {
 
   componentDidMount() {
     this.props.getUser();
@@ -19,7 +15,7 @@ class UserStream extends React.Component {
 
   render() {
     return <PhotoListView
-      photos={getpath(this, "props.stream")}
+      photos={getpath(this, "props.photos")}
       classes={this.props.classes}
       onScrollEnd={this.props.getUser}
     />
@@ -27,99 +23,13 @@ class UserStream extends React.Component {
 }
 
 
-@FlickUser()
-class UserFav extends React.Component {
-
-  componentDidMount() {
-    this.props.getFav();
-  }
-
-  render() {
-    return <PhotoListView
-      photos={getpath(this, "props.fav")}
-      classes={this.props.classes}
-      onScrollEnd={this.props.getFav}
-    />
-  }
-}
-
-
-@FlickUser()
-class UserAlbum extends React.Component {
-
-  componentDidMount() {
-    this.props.getAlbum();
-  }
-
-  render() {
-    return <AlbumListView
-      albums={getpath(this, "props.albums",[])}
-      classes={this.props.classes}
-      onScrollEnd={this.props.getAlbum}
-    />
-  }
-}
-
-
-
-/**
- * @class
- * @extends {React.Component<{user:{user: FlickrUserObj}} & ClassesProps>}
- */
-@FlickUser()
-class UserCover extends React.Component {
-
-  componentDidMount() {
-    this.props.getUser({ get_user_info: true });
-  }
-
-  render() {
-    const { classes } = this.props
-    /**@type {FlickrUserObj} */
-    const userInfo = getpath(this, "props.info", {});
-    const { retina, large, medium, default: ddd } = userInfo.iconurls || {}
-    const { h, l, s, t } = userInfo.coverphoto_url || {};
-    return <div className={classes.coverroot} style={{ backgroundImage: `url(${h || l || s || t})` }}>
-      <div className={classes.info}>
-        <img src={retina || large || medium || ddd} className={classes.avatar} />
-        <div className={classes.text}>
-          <div className={classes.line1}>
-            <span className={classes.name}>{(userInfo.realname || {})._content}</span>
-          </div>
-          <div className={classes.line2}>
-            {/* <span className={classes.des}>{(userInfo.description || {})._content}</span> */}
-          </div>
-        </div>
-      </div>
-    </div>
-  }
-}
-
-
-
-
 @withSCSS('./user.scss')
 export default class UserContainer extends React.Component {
-
   render() {
     const { match: { params: { userid } }, location: { hash }, classes } = this.props
     const tabValue = ["", "#album", "#fav"].indexOf(hash)
     return <div className={classes.root}>
-      <UserCover userid={userid} classes={classes} />
-      <Tabs className={classes.nav} value={tabValue > -1 ? tabValue : 0}>
-        <Tab label="Photo Stream" component={LangLink} to={`/flickr/u/${userid}`} />
-        <Tab label="Albums" component={LangLink} to={`/flickr/u/${userid}#album`} />
-        <Tab label="Faves" component={LangLink} to={`/flickr/u/${userid}#fav`} />
-      </Tabs>
-      <SwipeableViews
-        axis='x'
-        index={tabValue}
-        animateHeight
-      >
-        <UserStream userid={userid} classes={classes} />
-        <UserAlbum userid={userid} classes={classes} />
-        <UserFav userid={userid} classes={classes} />
-      </SwipeableViews>
+      <Album userid={userid} classes={classes} />
     </div >
   }
 }
